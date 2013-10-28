f79f05360af7800b8612c83c85765471319e29f2
NirajVirani-BTC <niraj.virani@botreeconsulting.com>
2013-10-28 14:58:13 +0530

    Sample front page

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -8,7 +8,7 @@
 Spree.config do |config|
   # Example:
   # Uncomment to override the default site name.
-  # config.site_name = "Spree Demo Site"
+  config.site_name = "Greetingo"
 end
 
 Spree.user_class = "Spree::User"
